6ed9c967fe0132cd68566eeb93b5a68a014e9573
Geovani Gustavo Sajorato Succi <geovanis@ciandt.com>
2021-09-23 16:30:59 -0300

    zuul api gateway created

---
+++ hr-api-gateway/src/main/java/com/br/octo/hrapigateway/HrapigatewayApplication.java
@@ -0,0 +1,13 @@
+package com.br.octo.hrapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrapigatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrapigatewayApplication.class, args);
+	}
+
+}
---
+++ hr-api-gateway/src/test/java/com/br/octo/hrapigateway/HrapigatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.octo.hrapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrapigatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
