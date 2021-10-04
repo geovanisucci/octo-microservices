8fc3ea24cc2ccf6388e6791ce531edffcdad97f6
geovanis <geovanis@ciandt.com>
2021-10-04 15:42:46 -0300

    oauth project created

---
+++ hr-oauth/src/main/java/com/br/octo/hroauth/HrOAuthApplication.java
@@ -0,0 +1,14 @@
+package com.br.octo.hroauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootApplication
+public class HrOAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HrOAuthApplication.class, args);
+    }
+}
