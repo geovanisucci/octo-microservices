25ea551adae95dc4896877819d9997b648a948bd
geovanis <geovanis@ciandt.com>
2021-10-04 11:46:27 -0300

    fix

--- hr-config-server/src/main/java/com/br/octo/hrconfigserver/HrConfigServerApplication.java
+++ hr-config-server/src/main/java/com/br/octo/hrconfigserver/HrConfigServerApplication.java
@@ -8,17 +8,18 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class HrConfigServerApplication implements CommandLineRunner {
+//public class HrConfigServerApplication implements CommandLineRunner {
+public class HrConfigServerApplication{
 
-    @Value("${spring.cloud.config.server.git.username}")
-    private String userName;
+    //@Value("${spring.cloud.config.server.git.username}")
+    //private String userName;
 
     public static void main(String[] args) {
         SpringApplication.run(HrConfigServerApplication.class, args);
     }
 
-    @Override
-    public void run(String... args) throws Exception {
-        System.out.println("USERNAME = " + userName);
-    }
+    //@Override
+    //public void run(String... args) throws Exception {
+      //  System.out.println("USERNAME = " + userName);
+    //}
 }
